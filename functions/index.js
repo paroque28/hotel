b56aca4843bdfdd48d1a94857bfb0db108e3862f
Pablo Rodriguez <paroque28@gmail.com>
2019-06-14 20:02:58 -0600

    Face detection

--- functions/index.js
+++ functions/index.js
@@ -19,7 +19,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 const mkdirp = require('mkdirp-promise');
-const vision = require('@google-cloud/vision')();
+const vision = require('@google-cloud/vision');
 const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
@@ -33,39 +33,42 @@ exports.blurOffensiveImages = functions.storage.object().onFinalize(async (objec
   const file = admin.storage().bucket(object.bucket).file(object.name);
 
   // Check the image content using the Cloud Vision API.
-  const data = await vision.detectSafeSearch(file);
-  const safeSearch = data[0];
-  console.log('SafeSearch results on image', safeSearch);
+  const client = new vision.ImageAnnotatorClient();
 
-  if (safeSearch.adult || safeSearch.violence) {
-    return blurImage(object.name, object.bucket, object.metadata);
-  }
+  const results = await client.faceDetection(file);
+  const faces = results[0].faceAnnotations;
+
+  const numFaces = faces.length;
+  console.log(`Found ${numFaces} face${numFaces === 1 ? '' : 's'}.`);
+
+  console.log('Face detection on image', faces);
+
   return null;
 });
 
-/**
- * Blurs the given image located in the given bucket using ImageMagick.
- */
-async function blurImage(filePath, bucketName, metadata) {
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-  const bucket = admin.storage().bucket(bucketName);
+// /**
+//  * Blurs the given image located in the given bucket using ImageMagick.
+//  */
+// async function blurImage(filePath, bucketName, metadata) {
+//   const tempLocalFile = path.join(os.tmpdir(), filePath);
+//   const tempLocalDir = path.dirname(tempLocalFile);
+//   const bucket = admin.storage().bucket(bucketName);
 
-  // Create the temp directory where the storage file will be downloaded.
-  await mkdirp(tempLocalDir);
-  console.log('Temporary directory has been created', tempLocalDir);
-  // Download file from bucket.
-  await bucket.file(filePath).download({destination: tempLocalFile});
-  console.log('The file has been downloaded to', tempLocalFile);
-  // Blur the image using ImageMagick.
-  await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x8', tempLocalFile]);
-  console.log('Blurred image created at', tempLocalFile);
-  // Uploading the Blurred image.
-  await bucket.upload(tempLocalFile, {
-    destination: filePath,
-    metadata: {metadata: metadata}, // Keeping custom metadata.
-  });
-  console.log('Blurred image uploaded to Storage at', filePath);
-  fs.unlinkSync(tempLocalFile);
-  console.log('Deleted local file', filePath);
-}
+//   // Create the temp directory where the storage file will be downloaded.
+//   await mkdirp(tempLocalDir);
+//   console.log('Temporary directory has been created', tempLocalDir);
+//   // Download file from bucket.
+//   await bucket.file(filePath).download({destination: tempLocalFile});
+//   console.log('The file has been downloaded to', tempLocalFile);
+//   // Blur the image using ImageMagick.
+//   await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x8', tempLocalFile]);
+//   console.log('Blurred image created at', tempLocalFile);
+//   // Uploading the Blurred image.
+//   await bucket.upload(tempLocalFile, {
+//     destination: filePath,
+//     metadata: {metadata: metadata}, // Keeping custom metadata.
+//   });
+//   console.log('Blurred image uploaded to Storage at', filePath);
+//   fs.unlinkSync(tempLocalFile);
+//   console.log('Deleted local file', filePath);
+// }
