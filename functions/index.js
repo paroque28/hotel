9a96c55a8fad93b2d97aa33d32a9dfc64c80a2d2
Pablo Rodriguez <paroque28@gmail.com>
2019-06-14 20:40:32 -0600

    Firebase first approach

--- functions/index.js
+++ functions/index.js
@@ -18,18 +18,20 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
-const mkdirp = require('mkdirp-promise');
 const vision = require('@google-cloud/vision');
-const spawn = require('child-process-promise').spawn;
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
 
-/**
- * When an image is uploaded we check if it is flagged as Adult or Violence by the Cloud Vision
- * API and if it is we blur it using ImageMagick.
- */
-exports.blurOffensiveImages = functions.storage.object().onFinalize(async (object) => {
+// Import the Firebase base SDK.
+const firebase = require('firebase/app');
+// Load the Firebase database module.
+// Here you should load all modules of Firebase that you need.
+require('firebase/database');
+
+// We initialize Firebase using a client-side config.
+const firebaseConfig = require('./firebase-config.json').result;
+(firebase.default || firebase).initializeApp(firebaseConfig);
+
+
+exports.reviewMood = functions.storage.object().onFinalize(async (object) => {
   // Check the image content using the Cloud Vision API.
   const client = new vision.ImageAnnotatorClient();
 
@@ -41,32 +43,17 @@ exports.blurOffensiveImages = functions.storage.object().onFinalize(async (objec
 
   console.log('Face detection on image', faces);
 
+  var message = "No expression detected!"
+  if(faces.joyLikelihood === 'VERY_LIKELY') message = "New Review: Joy :) 5 stars"
+  else  if(faces.sorrowLikelihood === 'VERY_LIKELY') message = "New Review: Sorrow :/ 2 stars"
+  else  if(faces.angerLikelihood === 'VERY_LIKELY') message = "New Review: Anger Dx 1 star"
+  else  if(faces. surpriseLikelihood === 'VERY_LIKELY') message = "New Review: Surprise :o 4 stars"
+
+
+  firebase.database().ref('messages').firebase.messages().push({
+    text: message,
+    userId: "AI",
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  });
   return null;
 });
-
-// /**
-//  * Blurs the given image located in the given bucket using ImageMagick.
-//  */
-// async function blurImage(filePath, bucketName, metadata) {
-//   const tempLocalFile = path.join(os.tmpdir(), filePath);
-//   const tempLocalDir = path.dirname(tempLocalFile);
-//   const bucket = admin.storage().bucket(bucketName);
-
-//   // Create the temp directory where the storage file will be downloaded.
-//   await mkdirp(tempLocalDir);
-//   console.log('Temporary directory has been created', tempLocalDir);
-//   // Download file from bucket.
-//   await bucket.file(filePath).download({destination: tempLocalFile});
-//   console.log('The file has been downloaded to', tempLocalFile);
-//   // Blur the image using ImageMagick.
-//   await spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x8', tempLocalFile]);
-//   console.log('Blurred image created at', tempLocalFile);
-//   // Uploading the Blurred image.
-//   await bucket.upload(tempLocalFile, {
-//     destination: filePath,
-//     metadata: {metadata: metadata}, // Keeping custom metadata.
-//   });
-//   console.log('Blurred image uploaded to Storage at', filePath);
-//   fs.unlinkSync(tempLocalFile);
-//   console.log('Deleted local file', filePath);
-// }
